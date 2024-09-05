31fe6eb50858966f44bca24b358948e36eb47930
nicolas772 <nicolas.arayau@sansano.usm.cl>
2024-09-05 18:02:06 -0400

    se obtienen todos los autos desde la bd en tableVehicles

--- frontend/src/components/TableVehicles.jsx
+++ frontend/src/components/TableVehicles.jsx
@@ -1,11 +1,31 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
 import { MRT_Localization_ES } from 'mantine-react-table/locales/es';
 import { useNavigate } from 'react-router-dom';
-import cars from '../mockups/cars.json';
+import VehicleService from '../services/vehicle.service';
+import { Spinner } from 'flowbite-react';
 
 export default function TableVehicles () {
   const navigate = useNavigate();
+  const [vehicles, setVehicles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    VehicleService.getAllVehicles()
+      .then((response) => {
+        const vehiclesWithOwnerFullName = response.data.map((vehicle) => ({
+          ...vehicle,
+          ownerFullName: vehicle.owner ? `${vehicle.owner.name} ${vehicle.owner.last_name}` : 'Sin propietario'
+        }));
+        setVehicles(vehiclesWithOwnerFullName);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching vehicles:', error);
+        setLoading(false);
+      });
+  }, []);
+  
   const columns = useMemo(
     () => [
       {
@@ -25,13 +45,14 @@ export default function TableVehicles () {
         header: 'Año',
       },
       {
-        accessorKey: 'owner',
+        accessorKey: 'ownerFullName',  // Usamos este campo para filtros y búsqueda
         header: 'Propietario',
+        Cell: ({ cell }) => cell.getValue(), // Mostramos el valor directamente desde ownerFullName
       },
       {
         accessorKey: 'price',
         header: 'Precio',
-        Cell: ({ cell }) => `$${cell.getValue()}`
+        Cell: ({ cell }) => `$${cell.getValue()}`,
       },
     ],
     [],
@@ -39,16 +60,24 @@ export default function TableVehicles () {
 
   const table = useMantineReactTable({
     columns,
-    data: cars,
+    data: vehicles,
     localization: MRT_Localization_ES,
     mantineTableContainerProps: { sx: { maxHeight: '300px' } },
     enableStickyHeader: true,
     initialState: { density: 'xs' },
     mantineTableBodyRowProps: ({ row }) => ({
-      onClick: () => navigate(`/vehicles/${row.original.id}`), // Redirigir a una URL específica basada en el ID
-      sx: { cursor: 'pointer' }, // Cambiar el cursor para indicar que la fila es clicable
+      onClick: () => navigate(`/vehicles/${row.original.id}`),
+      sx: { cursor: 'pointer' },
     }),
   });
 
-  return <MantineReactTable table={table}/>;
-};
+  return (
+    <>
+      {
+        loading
+        ? (<Spinner className='m-8'/>)
+        : (<MantineReactTable table={table}/>)
+      }
+    </>
+  );
+}
